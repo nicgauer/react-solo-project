90203dfff5e12bc7aa536a929ff03013ce129797
nic <nicgauer@gmail.com>
2021-03-25 10:41:50 -0500

    Thuresday phase5 complete

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,15 @@
 const router = require('express').Router();
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+const { restoreUser, requireAuth } = require('../../utils/auth.js');
 
-router.post('/test', (req, res) => {
-    res.json({ requestBody: req.body });
-})
+const sessionRouter = require('./session');
+const usersRouter = require('./users');
+
+router.use('/session', sessionRouter);
+router.use('/users', usersRouter);
 
 module.exports = router;
+
+
---
+++ backend/routes/api/session.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const router = express.Router();
+
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie, restoreUser } = require('../../utils/auth');
+const { User } = require('../../db/models');
+
+//Log in route
+router.post('/', asyncHandler(async (req, res, next) => {
+    const { credential, password } = req.body;
+    const user = await User.login({ credential, password });
+
+    if(!user) {
+        const err = new Error('Login Failed!');
+        err.status = 401;
+        err.title = 'Login failed';
+        err.errors = [ 'The provided credentials were invalid' ]
+        return next(err);
+    }
+
+    await setTokenCookie(res, user);
+
+    return res.json({
+        user
+    })
+}))
+
+//Log out route
+router.delete('/', (req, res) => {
+    res.clearCookie('token');
+    return res.json({ message: 'success' })
+})
+
+//Restore user 
+router.get('/', restoreUser, (req, res) => {
+    const { user } = req;
+    if(user){
+        return res.json({ 
+            user: user.toSafeObject(),
+        })
+    } else return res.json({});
+})
+
+module.exports = router;
+
+
+
