1ff1874a17702ebb986f77ebe071e5ccda00db5a
nic <nicgauer@gmail.com>
2021-03-31 14:53:43 -0500

    functioning audio player!!!!!

---
+++ frontend/src/components/ReleasePage/AudioPlayer.js
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+const AudioPlayer = ({ song }) => {
+
+    return (
+        <div>
+            <audio controls src={song.songURL} />
+        </div>
+    )
+}
+
+export default AudioPlayer;
--- frontend/src/components/ReleasePage/ReleaseDisplay.js
+++ frontend/src/components/ReleasePage/ReleaseDisplay.js
@@ -1,4 +1,4 @@
-
+import AudioPlayer from './AudioPlayer';
 
 const ReleaseDisplay = ({ release }) => {
     const Artist = release;
@@ -7,6 +7,7 @@ const ReleaseDisplay = ({ release }) => {
     return (<div>
         <h2>{Release.name} {Artist.name}</h2>
         <img src={Release.coverURL} />
+        {Release && Release.Songs.map(song => <AudioPlayer key={song.id} song={song} />)}
     </div>)
 }
 
